568165ae0e51225472195af61c71b14a82d820b5
bkshin <bkshin@bkshins-Mac-Studio.local>
2024-06-28 01:08:41 +0900

    Initial commit

---
+++ src/test/java/com/example/sqlchecker/SqlCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqlchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlCheckerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
